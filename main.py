6e5b3a907a46ec9055a51055b7972e12f523da16
fato <faotknt@gmail.com>
2024-06-16 12:51:35 +0900

    データベース接続基底クラスと従業員テーブル操作クラスを実装、テストを追加

--- main.py
+++ main.py
@@ -8,18 +8,3 @@ from database.database import get_connection, create_table_employees
 def cli():
     pass
 
-@cli.command()
-def test_db_connection():
-    """DB接続テスト"""
-    conn = get_connection()
-    if conn:
-        click.echo("DB接続に成功")
-    else:
-        click.echo("DB接続に失敗")
-
-if __name__ == "__main__":
-    cli()
-
-@cli.command()
-def create_employee_table():
-    """employees テーブルを作成"""
---
+++ tests/test_database.py
@@ -0,0 +1,26 @@
+# python3
+# test_database.py
+
+import unittest
+import sqlite3
+from database.base import Database
+
+class TestDatabase(unittest.TestCase):
+    def test_database_connection(self):
+        """DB接続テスト"""
+        
+        # 正常系: 既存のDBファイルに接続できる
+        with Database("projecttime.db") as db:
+            self.assertIsInstance(db.conn, sqlite3.Connection) 
+            #接続オブジェクトがsqlite3.Connectionである事を確認
+
+        # 異常系: 存在しないDBに接続使用とすると例外が発生する
+        with self.assertRaises(sqlite3.OperationalError):
+            Database("non_existent.db")
+
+    def test_execute(self):
+        """SQLクエリ実行テスト"""
+
+        with Database("projecttime.db") as db:
+            cursor = db.execute("SELECT * FROM employees") # employees テーブルがある事前提
+            self.assertIsInstance(cursor, sqlite3.Cursor) # カーソルオブジェクトである事を確認
